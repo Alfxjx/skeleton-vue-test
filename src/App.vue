516af50696ea9c4d6db68be80e9e82352b74c700
AlfredPC <xjxtju@163.com>
2019-02-19 16:15:52 +0800

    skeleton done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="app">
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    <p class="text">this is a test of vue skeleton plugin</p>
+    <div class="img-wrapper left">
+      <ul class="list">
+        <li class="list-item" v-for="n in 4" :key="n">
+          <span class="grey-rect">
+            <img alt="Vue logo" src="./assets/logo.png">
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="img-wrapper right">
+      <ul class="list">
+        <li class="list-item" v-for="n in 4" :key="n">
+          <span class="grey-rect">
+            <img alt="Vue logo" src="./assets/logo.png">
+          </span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    text-align center
+    .text
+      font-size 30px
+      color green
+    .img-wrapper
+      background-color #f3f5f7
+      width 250px
+      &.left
+        float left
+        margin-left 20%
+      &.right
+        float right
+        margin-right 20%
+      .list
+        padding 0
+        .list-item
+          text-align center
+          list-style none
+          margin 50px 0 50px 0
+          .grey-rect
+            line-height 30px
+            font-size 30px
+            color grey
 </style>
---
+++ src/Skeleton.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="ske-app">
+    <img src="./assets/logo.png">
+    <div class="ske-text">正在加载请稍候....</div>
+    <div class="img-wrapper left">
+      <ul>
+        <li class="list-item" v-for="n in 4" :key="n">
+          <span class="grey-rect">加载中..</span>
+        </li>
+      </ul>
+    </div>
+    <div class="img-wrapper right">
+      <ul>
+        <li class="list-item" v-for="n in 4" :key="n">
+          <span class="grey-rect">Loading...</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: "Skeleton.vue"
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .ske-app
+    text-align center
+    .ske-logo
+      width 200px
+      height 200px
+    .ske-text
+      color grey
+      height 30px
+      font-size 30px
+      margin-bottom 20px
+    .img-wrapper
+      background-color #f3f5f7
+      width 250px
+      &.left
+        float left
+        margin-left 20%
+      &.right
+        float right
+        margin-right 20%
+      .list-item
+        text-align center
+        list-style none
+        margin 50px 50px 50px 0
+        .grey-rect
+          line-height 30px
+          font-size 30px
+          color grey
+</style>
