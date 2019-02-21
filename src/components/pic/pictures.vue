fffb2f2ebab28ae3835af8b18875d074700511d6
AlfredPC <xjxtju@163.com>
2019-02-21 15:02:07 +0800

    lazyload plugin

--- src/components/pic/pictures.vue
+++ src/components/pic/pictures.vue
@@ -2,9 +2,9 @@
   <div class="pic">
     <div class="img-wrapper">
       <ul class="list">
-        <li class="list-item" v-for="n in 5" :key="n">
+        <li class="list-item" v-for="(pic, index) in pics" :key="index">
           <span class="grey-rect">
-            <img alt="Vue logo" src="../../assets/logo.png" width="200" height="200">
+            <img alt="Vue logo" v-lazy="pic.jpg" width="200" height="200">
           </span>
         </li>
       </ul>
@@ -14,7 +14,23 @@
 
 <script type="text/ecmascript-6">
   export default {
-    name: "pictures"
+    name: "pictures",
+    data() {
+      return {
+        pics: [
+          {jpg: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=67417550,4286791513&fm=26&gp=0.jpg'},
+          {jpg: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1931036651,4245219960&fm=26&gp=0.jpg'},
+          {jpg: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4069885691,778005459&fm=26&gp=0.jpg'},
+          {jpg: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=434554320,423201916&fm=26&gp=0.jpg'},
+          {jpg: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1421612651,3057937899&fm=26&gp=0.jpg'},
+          {jpg: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2497641768,2462225633&fm=26&gp=0.jpg'},
+          {jpg: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3677257638,2521150679&fm=26&gp=0.jpg'},
+          {jpg: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2767652891,3508546112&fm=26&gp=0.jpg'},
+          {jpg: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=341537136,3096421836&fm=26&gp=0.jpg'},
+          {jpg: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2589755639,4170511511&fm=26&gp=0.jpg'},
+        ]
+      }
+    }
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyLoad, {
+  error:'./static/error.jpg',
+  loading:'./static/loading.gif'
+})
 
 new Vue({
   render: h => h(App),
