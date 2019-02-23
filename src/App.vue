12bfd381da15c52a6dc93ade459b13b3b9d92b84
AlfredPC <xjxtju@163.com>
2019-02-23 11:45:51 +0800

    mock vue-markdown

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,23 @@
   <div id="app" class="app">
     <img class="logo" alt="Vue logo" src="./assets/logo.png">
     <p class="text">this is a test of vue skeleton plugin</p>
+    <markdown :mdString="content"></markdown>
     <pictures></pictures>
   </div>
 </template>
 
 <script>
 import pictures from './components/pic/pictures'
+import markdown from './components/mkd/markdown'
 export default {
   name: 'app',
+  data(){
+    return{
+      content: '## **hello ~~world~~**!'
+    }
+  },
   components:{
-    pictures
+    pictures, markdown
   }
 }
 </script>
---
+++ src/components/mkd/markdown.vue
@@ -0,0 +1,22 @@
+<template>
+  <vue-markdown>{{mdString}}</vue-markdown>
+</template>
+
+<script type="text/ecmascript-6">
+  import VueMarkdown from 'vue-markdown'
+
+  export default {
+    name: "markdown",
+    props:['mdString'],
+    data() {
+      return {}
+    },
+    components: {
+      VueMarkdown
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
